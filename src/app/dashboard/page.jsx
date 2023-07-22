d0c8f28f1b4f9562d9de5187c081031a5ea742c4
JannatunNimeNishat <nishatnime100@gmail.com>
2023-07-22 13:25:47 +0600

    side bar compited

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,21 +1,28 @@
 "use client"
 import NavBar from '@/component/NavBar/NavBar';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import plus from '../../../public/dashboard/side_nav_bar/plus.png'
+
 import dashboardIcon from '../../../public/dashboard/side_nav_bar/darhboardIcon.png'
 import up from '../../../public/dashboard/side_nav_bar/Expand_up.png'
+import down from '../../../public/dashboard/side_nav_bar/Expand_down.png'
 
 import Image from 'next/image';
+
+
+
 const dashboardPage = () => {
 
+    const [display, setDisplay] = useState(false)
+    console.log(display);
     return (
         <div>
             <div className='h-[50px]  flex items-center'>
                 <NavBar></NavBar>
             </div>
             <div className='grid grid-cols-7'>
-                <div className='col-span-1 bg-[#262626] flex flex-col justify-center items-center '>
+                <div className='col-span-1 bg-[#262626] flex flex-col justify-center items-center  '>
                     {/* new Item */}
                     <br />
                     <button className=' px-5 py-2 bg-white text-black flex gap-1 rounded-md items-center justify-center'>
@@ -25,16 +32,19 @@ const dashboardPage = () => {
 
                     <div className='h-1 bg-black w-full my-5'>
                     </div>
-                    {/* <hr className='my-5 bg-black text-black'/> */}
-
-                    <div className='flex gap-3  items-center justify-between '>
+                   {/* 1st */}
+                    <div className='flex gap-3 w-full px-5  items-center justify-between cursor-pointer'
+                    onClick={()=> setDisplay(!display)}
+                    >
                         <div className='flex gap-2'>
                             <Image src={dashboardIcon} alt='dashboardIcon'></Image>
                             <p>Dashboard</p>
                         </div>
-                        <Image src={up} alt='up'></Image>
+                        <Image  src={up} alt='up'></Image>
                     </div>
-                    <div className='text-left space-y-2 pt-2'>
+                    <div  className={`text-left space-y-2 pt-2 ${display === false ? 'hidden' : 'block'}`}
+                    
+                    >
                         <p>Commarce</p>
                         <p>Analytics</p>
                         <p>Crypto</p>
@@ -43,8 +53,83 @@ const dashboardPage = () => {
                         <p>Fitnes</p>
                     </div>
 
+                    <div className='h-1 bg-black w-full my-4'>
+                    </div>
+                   {/* 2nd */}
+                    <div className='flex gap-3 pb-2 w-full px-5   items-center justify-between '>
+                        <div className='flex gap-2'>
+                            <Image src={dashboardIcon} alt='dashboardIcon'></Image>
+                            <p>Application</p>
+                        </div>
+                        <Image src={down} alt='down'></Image>
+                    </div>
+                    <div className='h-1 bg-black w-full my-4'>
+                    </div>
+                   {/* 3rd */}
+                    <div className='flex gap-3 pb-2 w-full px-5 items-center justify-between '>
+                        <div className='flex gap-2'>
+                            <Image src={dashboardIcon} alt='dashboardIcon'></Image>
+                            <p>Elements</p>
+                        </div>
+                        <Image src={down} alt='down'></Image>
+                    </div>
 
+                    <div className='h-1 bg-black w-full my-4'>
+                    </div>
+                   {/* 4th */}
+                    <div className='flex gap-3 pb-2 w-full px-5   items-center justify-between '>
+                        <div className='flex gap-2'>
+                            <Image src={dashboardIcon} alt='dashboardIcon'></Image>
+                            <p>Forms</p>
+                        </div>
+                        <Image src={down} alt='down'></Image>
+                    </div>
+                    <div className='h-1 bg-black w-full my-4'>
+                    </div>
+                   {/* 5th */}
+                    <div className='flex gap-3 pb-2 w-full px-5  items-center justify-between '>
+                        <div className='flex gap-2'>
+                            <Image src={dashboardIcon} alt='dashboardIcon'></Image>
+                            <p>Plugins</p>
+                        </div>
+                        <Image src={down} alt='down'></Image>
+                    </div>
+                    <div className='h-1 bg-black w-full my-4'>
+                    </div>
+                   {/* 6th */}
+                    <div className='flex gap-3 pb-2 w-full px-5  items-center justify-between '>
+                        <div className='flex gap-2'>
+                            <Image src={dashboardIcon} alt='dashboardIcon'></Image>
+                            <p>Elements</p>
+                        </div>
+                        <Image src={down} alt='down'></Image>
+                    </div>
+                    <div className='h-1 bg-black w-full my-4'>
+                    </div>
+                   {/* 7th */}
+                    <div className='flex gap-3 pb-2 w-full px-5  items-center justify-between '>
+                        <div className='flex gap-2'>
+                            <Image src={dashboardIcon} alt='dashboardIcon'></Image>
+                            <p>Datagrid</p>
+                        </div>
+                        <Image src={down} alt='down'></Image>
+                    </div>
+                    <div className='h-1 bg-black w-full my-4'>
+                    </div>
+                   {/* 8th */}
+                    <div className='flex gap-3 pb-2 w-full px-5  items-center justify-between '>
+                        <div className='flex gap-2'>
+                            <Image src={dashboardIcon} alt='dashboardIcon'></Image>
+                            <p>Setting</p>
+                        </div>
+                        <Image src={down} alt='down'></Image>
+                    </div>
+                   
                 </div>
+
+
+
+
                 <div className='col-span-6'>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos magnam voluptatibus obcaecati voluptatem, iure porro iste illum incidunt magni at, inventore, quaerat perspiciatis. Animi delectus nisi repellat dolorem necessitatibus optio.
                 </div>
